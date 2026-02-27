b29479c34d19f2d4a31cc659a5d413168151965e
user.email <andre23dev@gmail.com>
2026-02-27 18:24:17 -0300

    Adicionado funções

--- 02-praticando-projetos/contador.py
+++ 02-praticando-projetos/contador.py
@@ -1,4 +1,11 @@
-frase = input("Digite uma frase: ")
-palavras = frase.split()
-print(len(palavras))
-print(palavras)
+def limpar_texto(texto):
+    texto = texto.lower()
+    caracteres = ",.!?;:\"'()[]{}"
+    for char in caracteres:
+        texto = texto.replace(char, "")
+    return texto
+
+def contar_palavras(frase):
+    frase = limpar_texto(frase)
+    palavras = frase.split()
+    return len(palavras)
