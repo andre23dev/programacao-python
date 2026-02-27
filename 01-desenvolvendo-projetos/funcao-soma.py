aca6203f610412ced1894778921c240a407b3ebc
user.email <andre23dev@gmail.com>
2026-02-27 18:01:01 -0300

    Desenvolvendo projetos

---
+++ 01-desenvolvendo-projetos/funcao-soma.py
@@ -0,0 +1,8 @@
+def somar(a, b):
+    return a + b
+
+numero1 = float(input("Digite um número >> "))
+numero2 = float(input("Digite outro número >> "))
+
+resultado = somar(numero1, numero2)
+print(f"O resultado da soma é: {resultado}")
